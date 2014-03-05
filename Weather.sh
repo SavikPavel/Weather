ecac5c0569785851d804f916c63d2413a055ad6d
SavikPavel <savikpavel@gmail.com>
2014-03-05 18:22:12 +0300

    Added shell file and config

---
+++ Weather.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+. $PWD/Weather.conf
+
+while (true)
+do
+	#if [ -f $PAGEFILE ]; then rm $PAGEFILE; fi
+	wget -t 1 -O $PAGEFILE -o wget.log http://meteo.by/
+	grep -m 1 '<span class="max">' $PAGEFILE > temp
+	echo "temperature: " $(sed 's/[^\-\+0-9]//g' temp)
+	rm temp
+	sleep $DELAY
+done
